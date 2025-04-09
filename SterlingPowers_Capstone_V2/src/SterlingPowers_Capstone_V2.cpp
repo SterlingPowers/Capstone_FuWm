d957a1fdd2187e07c0ea88ef4771f6c13cdaa6ce
Admin <ms.sterling.powers@gmail.com>
2025-04-09 17:47:02 -0600

    button for tracks

--- SterlingPowers_Capstone_V2/src/SterlingPowers_Capstone_V2.cpp
+++ SterlingPowers_Capstone_V2/src/SterlingPowers_Capstone_V2.cpp
@@ -8,12 +8,17 @@
  #include "MP3_CNM.h"
  #include "Button.h"
  
- Button nextButton(A5);  // A5 connected to button, other side to GND
+ Button nextButton(A5);  
  DFRobotDFPlayerMini thePlayer;
+ const int laser1=A0;
+ const int diode1=A1;
  
  SYSTEM_MODE(SEMI_AUTOMATIC);
  
  void setup() {
+  pinMode (laser1, OUTPUT);
+  pinMode (diode1, INPUT);
+
    Serial.begin(9600);
    waitFor(Serial.isConnected, 1000);
    Serial1.begin(9600);
@@ -21,7 +26,7 @@
  
    Serial.println("Welcome to FuWm");
    Serial.println("About to init DFPlayer...");
-   
+
    if (!thePlayer.begin(Serial1)) {
      Serial.println("Hold up. I'm having issues...");
      while (true);
@@ -30,11 +35,20 @@
  
    thePlayer.volume(30);  // Range: 0–30
    thePlayer.play(1);     // Starts with track 1
+   thePlayer.enableLoopAll ();
+
  }
  
  void loop() {
+  digitalWrite (laser1, HIGH);
+  delay (100);
+
+  int sensorValue = analogRead (diode1);
+  Serial.printf("light level: %d\n", sensorValue);
+
    if (nextButton.isClicked()) {
      Serial.println("Next Song");
+     thePlayer.next();
      thePlayer.next();
    }
  }
